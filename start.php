9e0340f8fea46d9728a6108bef86df5f8b63ec5d
sebastien Deren <sebastien.deren@gmail.com>
2022-08-06 19:07:32 +0200

    Modification mineur du CSS et du html (ajout d'une bordure blanche en fin de section de menu) ajout du projet en php (reprise de la page index et suppression des elements non necessaires) ajout des fichier header et nav

---
+++ start.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style/style.css"/>
+        <title>Marché de pro</title>
+    </head>
+    <body id="page">
+
+        <div id="corps_texte">
+            <div id="left">
+                <header id="tete">
+                    <!-- afficher une demande de connexion si pas connecté
+                        si connecté affiché l'icone choisi par l'utilisateur + son nom +titre
+                        sur le modéle si dessous -->
+
+                    <!--<div id="photoID">
+                        <img class="photo" src="./images/avatar.jpg" alt="ma photo"/>
+                    </div>
+                    <div id="presentation">
+                        <h1>Sébatien Deren</h1>
+                        <h2>apprenti developeur Web</h2>
+                    </div>-->
+                </header>
+                <nav id="menu">
+                    <section class="cat_nav">
+                    <a href="index.html"><div class="element 1">
+                        <img src="images/accueil.png" alt="maison" class="icon"/>
+                        Home
+                    </div></a>
+                    </section>
+                    <section class="cat_nav">
+                        <!-- rechercher les categories d'aliment dans la base de donnée et créer un lien pour chaque-->
+                    </section> 
+                </nav>
+            </div>
+            <div id="right">
+                <?php if
+                <!-- creer pour chaque catégorie une page de choix d'article-->
+            </div>
+        </div>
+    </body>
+
+</html>
