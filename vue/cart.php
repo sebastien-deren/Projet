95949df391d5eb4059e4825d7da6c42a3279083b
sebastien <sebastien.deren@gmail.com>
2022-09-29 14:43:23 +0200

    supression de code périmé

--- vue/cart.php
+++ vue/cart.php
@@ -1,15 +1,4 @@
 <?php
-$test = 4;
-$MAX_ID = get_max_id_products();
-for ($i = 0; $i <= $MAX_ID; $i++) {
-
-    if (isset($_POST["quantity" . $i])) {
-        add_cart($i, $_POST["quantity" . $i]);
-    }
-}
-if (isset($_POST['supprimer'])) {
-    delete_item_in_cart_db($_POST['id_product']);
-}
 $cart = get_cart_db();
 
 ?>
